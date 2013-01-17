f5c6ff48acf4419bc02602cab9fe5c3503908886
Tim Brody <tdb2@ecs.soton.ac.uk>
2013-01-17 16:52:00 +0000

    Chunked sizes are in HEX

--- lib/EventEmitter/HTTP/Request.pm
+++ lib/EventEmitter/HTTP/Request.pm
@@ -4,6 +4,8 @@ use base qw( HTTP::Request EventEmitter );
 
 use strict;
 
+use constant CRLF => "\015\012";
+
 sub write
 {
 	my ($self, $data) = @_;
@@ -11,9 +13,12 @@ sub write
 	$self->add_content($data);
 
 	if ($self->{_connection} && length ${$self->content_ref}) {
-		$self->{_connection}->push_write(length(${$self->content_ref})."\r\n");
+		$self->{_connection}->push_write(sprintf('%x%s',
+			length(${$self->content_ref}),
+			CRLF
+		));
 		$self->{_connection}->push_write(${$self->content_ref});
-		$self->{_connection}->push_write("\r\n");
+		$self->{_connection}->push_write(CRLF);
 		$self->content("");
 	}
 }
@@ -31,7 +36,7 @@ sub end
 	}
 
 	$self->write(""); # write any buffered data
-	$self->{_connection}->push_write("0\r\n\r\n");
+	$self->{_connection}->push_write('0'.CRLF.CRLF);
 }
 
 1;
